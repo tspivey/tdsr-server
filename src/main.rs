155127de4ef9bbb8c862ee3d7ccf5097983ab94f
Quin Gillespie <TheQuinbox@gmail.com>
2025-03-17 09:16:24 -0600

    Added a system tray icon (#4)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![windows_subsystem = "windows"]
+use native_dialog::{MessageDialog, MessageType};
 use std::{
     error::Error,
     io::{BufRead, BufReader},
@@ -6,33 +8,76 @@ use std::{
     sync::{Arc, Mutex},
     thread,
 };
+use tao::{
+    event::Event,
+    event_loop::{ControlFlow, EventLoopBuilder},
+};
 use textwrap::{Options, wrap};
+use tray_icon::{
+    TrayIconBuilder,
+    menu::{Menu, MenuEvent, MenuItem},
+};
 use tts::Tts;
 
 type TtsRef = Arc<Mutex<Result<Tts, tts::Error>>>;
 
+enum UserEvent {
+    MenuEvent(tray_icon::menu::MenuEvent),
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let listener = TcpListener::bind("0.0.0.0:64111").map_err(|error| {
-        eprintln!("Unable to bind: {:?}", error);
+        show_error(&format!("Unable to bind: {:?}", error));
         process::exit(1);
     })?;
     let tts = Arc::new(Mutex::new(Tts::default()));
-    for connection in listener.incoming() {
-        let tts = Arc::clone(&tts);
-        thread::spawn(move || {
-            let connection = match connection {
-                Ok(conn) => conn,
-                Err(e) => {
-                    eprintln!("Failed to accept connection: {}", e);
-                    return;
+    thread::spawn(move || {
+        for connection in listener.incoming() {
+            let tts = Arc::clone(&tts);
+            thread::spawn(move || {
+                let connection = match connection {
+                    Ok(conn) => conn,
+                    Err(e) => {
+                        show_error(&format!("Failed to accept connection: {}", e));
+                        return;
+                    }
+                };
+                if let Err(e) = handle_connection(connection, &tts) {
+                    show_error(&format!("Error handling connection: {}", e));
                 }
-            };
-            if let Err(e) = handle_connection(connection, &tts) {
-                eprintln!("Error handling connection: {}", e);
+            });
+        }
+    });
+    let event_loop = EventLoopBuilder::<UserEvent>::with_user_event().build();
+    let proxy = event_loop.create_proxy();
+    MenuEvent::set_event_handler(Some(move |event| {
+        let _ = proxy.send_event(UserEvent::MenuEvent(event));
+    }));
+    let mut tray_icon = None;
+    let tray_menu = Menu::new();
+    let quit_i = MenuItem::new("&Quit", true, None);
+    tray_menu.append(&quit_i)?;
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        match event {
+            Event::NewEvents(tao::event::StartCause::Init) => {
+                tray_icon = Some(
+                    TrayIconBuilder::new()
+                        .with_menu(Box::new(tray_menu.clone()))
+                        .with_tooltip("TDSR Server")
+                        .build()
+                        .unwrap(),
+                );
             }
-        });
-    }
-    Ok(())
+            Event::UserEvent(UserEvent::MenuEvent(event)) => {
+                if event.id == quit_i.id() {
+                    tray_icon.take();
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => {}
+        }
+    });
 }
 
 fn handle_connection(connection: TcpStream, tts: &TtsRef) -> Result<(), Box<dyn Error>> {
@@ -65,7 +110,7 @@ fn speak(text: &str, tts: &TtsRef) {
     if let Ok(mut tts) = tts.lock() {
         if let Ok(tts) = tts.as_mut() {
             if let Err(e) = tts.speak(text, true) {
-                eprintln!("Failed to speak: {}", e);
+                show_error(&format!("Failed to speak: {}", e));
             }
         }
     }
@@ -75,8 +120,16 @@ fn stop_speaking(tts: &TtsRef) {
     if let Ok(mut tts) = tts.lock() {
         if let Ok(tts) = tts.as_mut() {
             if let Err(e) = tts.stop() {
-                eprintln!("Failed to stop speaking: {}", e);
+                show_error(&format!("Failed to stop speaking: {}", e));
             }
         }
     }
+}
+
+fn show_error(message: &str) {
+    let _ = MessageDialog::new()
+        .set_title("Error")
+        .set_type(MessageType::Error)
+        .set_text(message)
+        .show_alert();
 }
